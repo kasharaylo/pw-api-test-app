36726bb23c57b87555a27a427dbfb3ec46c1c715
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-07-08 10:56:38 +0300

    Mocking API

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,10 +1,16 @@
 import {test, expect} from '@playwright/test';
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({page}) => {
-  await page.goto('https://conduit.bondaracademy.com/');
+    await page.route('*/**/api/tags', async route => { // Intercept the API request
+        // Fulfill the request with the mock data from tags.json
+        await route.fulfill({
+            body: JSON.stringify(tags),
+        });
+    });
+    await page.goto('https://conduit.bondaracademy.com/');
 })
 
 test('has title', async ({page}) => {
-    // Expect a title "to contain" a substring.
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 })
