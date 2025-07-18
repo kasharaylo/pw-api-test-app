b069d152e5ac59b516a106d52d04bf95c7e5e3e5
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-07-18 10:52:45 +0300

    Modify API Response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -8,9 +8,21 @@ test.beforeEach(async ({page}) => {
             body: JSON.stringify(tags),
         });
     });
+    
+    await page.route('*/**/api/articles*', async route => { // Intercept the API request
+        const responce = await route.fetch();
+        const responceBody = await responce.json();
+        responceBody.articles[0].title = 'Mocked Article Title'; // Modify the response
+        responceBody.articles[0].description = 'Mocked Article Description'; // Modify the response
+        await route.fulfill({
+            body: JSON.stringify(responceBody) // Return the modified response
+        });
+    });
     await page.goto('https://conduit.bondaracademy.com/');
-})
+});
 
 test('has title', async ({page}) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+    await expect(page.locator('.article-preview h1').first()).toContainText("Mocked Article Title");
+    await expect(page.locator('.article-preview p').first()).toContainText("Mocked Article Description");
 })
